0c4a28719a988b3700e5262e7a1af4187f944667
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2024-01-24 17:37:31 -0300

    ajustando código

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -7,17 +7,18 @@ namespace DesafioTrilhaNetPoo.Models
 {
      public abstract class Smartphone
     {
+        //Atributos
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
         private string Modelo { get; set; }
         private string IMEI { get; set; }
         private int Memoria { get; set; }
-        
-        
 
 
+
+        // Construtor
         public Smartphone(string numero, string modelo, string imei, int memoria)
-        {
+        {//parâmetros do tipo 
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
             Modelo = modelo;
@@ -25,15 +26,14 @@ namespace DesafioTrilhaNetPoo.Models
             Memoria = memoria;            
         }
 
-
-    
+        //Métodos
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string v)
         {
             Console.WriteLine("Recebendo ligação...");
         }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "987654321", modelo: "Nokia 3310", imei: "IMEI987", memoria: 36);
 nokia.Ligar();
 nokia.InstalarAplicativo("Nokia");
+nokia.ReceberLigacao("Nokia");
 
 Console.WriteLine("\n");
 
@@ -12,4 +13,5 @@ Console.WriteLine("Smartphone iPhone:");
 Smartphone iphone = new Iphone(numero: "123456789", modelo: "iPhone X", imei: "IMEI123", memoria: 64);
 iphone.Ligar();
 iphone.InstalarAplicativo("iPhone");
+iphone.ReceberLigacao("Iphome");
 
