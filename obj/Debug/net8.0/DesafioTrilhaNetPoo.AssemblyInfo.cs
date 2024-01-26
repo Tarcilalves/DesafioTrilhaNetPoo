3488bf8ea5f1a00818bdb7c3e1647f760a75e7c5
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2024-01-25 21:47:18 -0300

    Ajustes

--- obj/Debug/net8.0/DesafioTrilhaNetPoo.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesafioTrilhaNetPoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioTrilhaNetPoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01afd93cd9b02bf9804a8c7d2e80bf8948275c3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11809fe63225300e598df384067a654140c2b0c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioTrilhaNetPoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioTrilhaNetPoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
